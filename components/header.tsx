840e45cb8ecaccdd52fe27a93451241438ccc3b7
dhzdhd <ragas5432100@gmail.com>
2022-03-15 12:38:47 +0530

    Improve aesthetics of first section

--- components/header.tsx
+++ components/header.tsx
@@ -1,4 +1,4 @@
-import { Canvas, useFrame } from '@react-three/fiber';
+import { Canvas, useFrame, useLoader } from '@react-three/fiber';
 import { useRef, useState } from 'react';
 import { TextureLoader } from 'three';
 import styles from '../styles/components/Header.module.sass';
@@ -7,15 +7,12 @@ function Sphere(props: JSX.IntrinsicElements['mesh']) {
     const ref = useRef<THREE.Mesh>(null!)
     useFrame((state, delta) => (ref.current.rotation.x += 0.01))
 
-    const loader = new TextureLoader();
-    const texture = loader.load('../public/e.jpg');
-
     return (
       <mesh
         {...props}
         ref={ref}>
         <sphereGeometry />
-        <meshStandardMaterial color={0x63ffee} roughness={1} roughnessMap={texture} envMap={texture} />
+        <meshStandardMaterial color={0x63ffee} roughness={1} />
       </mesh>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -14,12 +14,13 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div>
-        <SocialsBar />
         <section className={styles.landing}>
-          <span>Hi, my name is</span>
-          <h1>dhzdhd</h1>
-          <h2>I build multiplatform apps</h2>
+          <h1 className={styles.h1}>Hi, my name is</h1>
+          <h2 className={styles.h2}>dhzdhd</h2>
+          <h3 className={styles.h3}>I build multiplatform apps.</h3>
+          <p className={styles.p}>I&apos;m a Data Science undergraduate student who has an interest in developing web, desktop and mobile apps using various frameworks and languages.</p>
         </section>
+        <SocialsBar />
       </div>
     </>
   )
